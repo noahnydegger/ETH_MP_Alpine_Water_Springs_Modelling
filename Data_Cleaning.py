4c91208b2898f1c2ae81b7261e47aa0a7a395a95
Noah Nydegger <noah.nydegger@gmail.com>
2023-11-01 16:55:12 +0100

    temperature plots and temperature correlation added. Peak finding adjusted: prominence = 10% of mean discharge

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -27,14 +27,16 @@ def add_timezone_mc(mc_u,mc_pf):
     mc_u['datetime'] = pd.to_datetime(mc_u['datetime']).dt.tz_localize('Europe/Zurich')
     return mc_u, mc_pf
 
-def resample_and_save_spring_data(df, resolutions, save_path, spring_name):
+
+def resample_and_save_spring_data(df, resolutions, save_path, spring_name, new_data_available):
     # create the folder for the spring if it does not exist yet
     spring_folder = os.path.join(save_path, spring_name)
     Helper.create_directory(spring_folder)
     # save the original valid data
     filename = f"{spring_name}_{'10min'}.csv"
     file_path = os.path.join(spring_folder, filename)
-    df.to_csv(file_path)
+    if new_data_available:
+        df.to_csv(file_path)
 
     # Convert the index to datetime if it's not already
     df.index = pd.to_datetime(df.index)
@@ -50,29 +52,32 @@ def resample_and_save_spring_data(df, resolutions, save_path, spring_name):
         # Save the resampled dataframe as a CSV file
         filename = f"{spring_name}_{resolution}.csv"
         file_path = os.path.join(spring_folder, filename)
-        resampled_df.to_csv(file_path)
+        if new_data_available:
+            resampled_df.to_csv(file_path)
 
     return resampled_dfs
 
 
-def resample_and_save_precip_data(df_list, resolutions, save_path, prefix):
+def resample_and_save_precip_data(df_list, resolutions, save_path, prefix, new_data_available):
     resampled_dfs = {}  # Dictionary to store the resampled dataframes
 
     # create the folder if it does not exist yet
     Helper.create_directory(save_path)
     # save the original 10 min data
     if 'rre150z0' in df_list[0].columns:
-        filename = f"{prefix}_{'10min'}.csv"
+        filename = f"{prefix}_{'precip_10min'}.csv"
         file_path = os.path.join(save_path, filename)
         df_10min = df_list[0].loc[:, ['rre150z0']]
-        df_10min.to_csv(file_path)
+        if new_data_available:
+            df_10min.to_csv(file_path)
         resampled_dfs['10min'] = df_10min
 
     # save the original hourly data
-    filename = f"{prefix}_{'H'}.csv"
+    filename = f"{prefix}_{'precip_H'}.csv"
     file_path = os.path.join(save_path, filename)
     df_H = df_list[1].loc[:, ['rre150h0']]
-    df_H.to_csv(file_path)
+    if new_data_available:
+        df_H.to_csv(file_path)
     resampled_dfs['H'] = df_H
 
     # Convert the index to datetime if it's not already
@@ -85,8 +90,50 @@ def resample_and_save_precip_data(df_list, resolutions, save_path, prefix):
         resampled_dfs[resolution] = resampled_df  # Store the resampled dataframe in the dictionary
 
         # Save the resampled dataframe as a CSV file
-        filename = f"{prefix}_{resolution}.csv"
+        filename = f"{prefix}_precip_{resolution}.csv"
         file_path = os.path.join(save_path, filename)
-        resampled_df.to_csv(file_path)
+        if new_data_available:
+            resampled_df.to_csv(file_path)
+
+    return resampled_dfs
+
+
+def resample_and_save_temp_data(df_list, resolutions, save_path, prefix, new_data_available):
+    resampled_dfs = {}  # Dictionary to store the resampled dataframes
+
+    # create the folder if it does not exist yet
+    Helper.create_directory(save_path)
+    # save the original 10 min data: is always the first dataframe in the list
+    df = df_list[0]
+    # get the column name containing the temperature data
+    substring = 'tre'
+    temp_column = next((col for col in df.columns if substring in col), None)
+    if temp_column is None:
+        return 'No temperature data available'
+
+    filename = f"{prefix}_{'temp_10min'}.csv"
+    file_path = os.path.join(save_path, filename)
+    df_temp = df[[temp_column]].rename(columns={temp_column: 'temperature(C)'})
+    #df_temp = df[temp_column]
+    #df_temp.rename(columns={temp_column: 'temperature(C)'}, inplace=True)
+
+    # Convert the index to datetime if it's not already
+    df_temp.index = pd.to_datetime(df_temp.index)
+    resampled_dfs['10min'] = df_temp
+
+    if new_data_available:
+        df_temp.to_csv(file_path)
+
+    for resolution in resolutions:
+        # Resample the data based on the specified resolution
+        resampled_df = df_temp.resample(resolution).mean()
+
+        resampled_dfs[resolution] = resampled_df  # Store the resampled dataframe in the dictionary
+
+        # Save the resampled dataframe as a CSV file
+        filename = f"{prefix}_temp_{resolution}.csv"
+        file_path = os.path.join(save_path, filename)
+        if new_data_available:
+            resampled_df.to_csv(file_path)
 
     return resampled_dfs
---
+++ s_preprocess4peaks.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import scipy.signal as ss
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+dpath = Path(".") / "Data" / "spring_data" / "wabesense_discharge_2023-09-01"
+fpath = dpath / "Oberriet.SS.Ulrika_discharge.csv"
+#fpath = dpath / "Bonaduz.BS.Paliu_Fravi_discharge.csv"
+#fpath = dpath / "Bonaduz.SS.Leo_Friedrich_discharge.csv"
+#fpath = dpath / "Hergiswil.BS.Rossmoos_discharge.csv"
+df = pd.read_csv(fpath)
+
+q = "discharge(L/min)"
+qs = "discharge_smooth"
+# do not forget to fill NA when needed by the algo
+df[qs] = df[q].rolling(window=12*60//10).mean().ffill().bfill()
+
+# get peaks
+
+peaks, _ = ss.find_peaks(df[qs], prominence=20, distance=24*60//10)
+pw, *_ = ss.peak_widths(df[qs], peaks, rel_height=0.2)
+
+print(pd.DataFrame({"peak_width [h]": pw*10/60}).describe())
+
+fig, ax = plt.subplots()
+df[[q, qs]].plot(ax=ax)
+ax.plot(peaks, df[qs][peaks], "ob")
+
+fig, ax = plt.subplots()
+ax.boxplot(pw * 10 / 60)
+ax.set_yscale("log")
+ax.set_ylabel("peak width [hr]")
+plt.show()
+
+
