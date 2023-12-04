596b46e9b267f46fae9b7f02349af1aa356fe2d4
JuanPi Carbajal <ajuanpi+dev@gmail.com>
2023-12-04 22:02:18 +0100

    example of sampling path from an AR model using statsmodels

--- AR_Model/example_ar_model.py
+++ AR_Model/example_ar_model.py
@@ -23,7 +23,9 @@
 import numpy as np
 from numpy.typing import ArrayLike
 import matplotlib.pyplot as plt
-from statsmodels.tsa.ar_model import AutoReg
+from statsmodels.tsa.ar_model import AutoReg, AutoRegResults
+
+rng = np.random.default_rng()
 
 
 def create_data(*, weights: ArrayLike, lags: ArrayLike,
@@ -56,6 +58,21 @@ def create_data(*, weights: ArrayLike, lags: ArrayLike,
     return data
 
 
+def sample_ar_path(res: AutoRegResults, *, xinit: np.ndarray, steps: int) -> np.ndarray:
+    """Sample a path from a fitted AR model.
+    """
+    coefs = res.params
+    if xinit.size < coefs.size - 1:
+        raise ValueError(f"Not enough initial values to start: {xinit.size} < {coefs.size}")
+    noise = rng.normal(loc=0, scale=np.sqrt(res.scale), size=steps-xinit.size)
+    lags = np.asarray(res.ar_lags)
+    path = np.zeros(steps)
+    path[:xinit.size] = xinit.copy()
+    for i in range(xinit.size, steps):
+        path[i] = path[i - lags] @ coefs[1:] + coefs[0] + noise[i-xinit.size]
+    return path
+
+
 if __name__ == "__main__":
     # Create data
     l = np.array([1, 3, 4, 5])
@@ -64,8 +81,7 @@ if __name__ == "__main__":
     x0 = np.array([0, 0.1, 0.2, -0.1, 0.3])
     data_latent = create_data(weights=w, lags=l, x0=x0, samples=100)
     # add noise
-    rng = np.random.default_rng()
-    factor = 0  # TODO: set this value to e.g. 0.05
+    factor = 0.1  # TODO: set this to different values
     data = data_latent + rng.normal(loc=0.0, scale=factor * np.abs(data_latent).max(),
                                     size=data_latent.shape)
     t = np.arange(data.size)
@@ -82,12 +98,20 @@ if __name__ == "__main__":
 
     data_ = res.predict(end=data.size-1, dynamic=trn_end)
 
+    # Sample some paths
+    Np = 25
+    paths = np.zeros((Np, data.size))
+    for n in range(Np):
+        paths[n] = sample_ar_path(res, xinit=x0, steps=data.size)
+    path_args = dict(color='r', linestyle='-', alpha=0.1, label="model paths")
     fig, ax = plt.subplots()
+    ax.plot(t, paths.T, **path_args)
     ax.plot(t, data_latent, "-", label="latent")
     ax.plot(t[:trn_end], trn, "o", label="training")
     ax.plot(t[trn_end:], tst, "x", label="testing")
-    ax.plot(t, data_, "--", label="model")
-    plt.legend()
+    ax.plot(t, data_, "b--", label="model")
+    hdl, lbl = ax.get_legend_handles_labels()
+    plt.legend(hdl[n:], lbl[n:])
 
     # using integrated plot function
     fig = res.plot_predict(start=0, end=data.size-1)
@@ -95,6 +119,8 @@ if __name__ == "__main__":
     ax.plot(t, data_latent, ":", label="latent")
     ax.plot(t[:trn_end], trn, "o", label="training")
     ax.plot(t[trn_end:], tst, "x", label="testing")
-    plt.legend()
+    ax.plot(t, paths.T, **path_args)
+    hdl, lbl = ax.get_legend_handles_labels()
+    plt.legend(hdl[:5], lbl[:5])
 
     plt.show()
